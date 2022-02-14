79df389e6859748bf2b7aec1d2d1a23c64e8cc48
Jon Grim <jonjongrim@gmail.com>
2022-02-13 21:24:24 -0500

    get basic tests passing (#1)
    * get basic tests passing
    
    * remove extra brackets
    
    * add env to yml

---
+++ tests/e2e/integration/navigation.ts
@@ -0,0 +1,13 @@
+describe("Navbar", () => {
+  it("Has a link to community", () => {
+    cy.visit("/");
+    cy.findByRole("link", { name: /Communities/ }).click();
+    cy.findByRole("heading", { name: /Grimlin Games/i });
+  });
+
+  it("Has a link to games", () => {
+    cy.visit("/");
+    cy.findByRole("link", { name: /Games/ }).click();
+    cy.url().should("eq", "http://localhost:3000/games/browse");
+  });
+});
