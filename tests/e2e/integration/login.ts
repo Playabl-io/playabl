da34b42ac823605c02a3f6aa9742a9a4d20f1b78
Jonathan Grim <jonjongrim@gmail.com>
2022-02-14 23:06:35 -0500

    click sign in

--- tests/e2e/integration/login.ts
+++ tests/e2e/integration/login.ts
@@ -4,6 +4,7 @@ describe("Login", () => {
     cy.findByText("Sign in").click();
     cy.findByRole("textbox", { name: /Email/ }).type("jonjongrim@gmail.com");
     cy.findByLabelText("Password").type(Cypress.env("PLAYABL_PW"));
+    cy.findByRole("button", { name: "Sign in" }).click();
     cy.url().should("eq", "http://localhost:3000/profile");
   });
 });
