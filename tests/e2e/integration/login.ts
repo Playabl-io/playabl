0f7ab5262889e6e943c2eddf7b0882779f09d5f2
Jonathan Grim <jonjongrim@gmail.com>
2022-02-14 22:56:02 -0500

    update cypres env

--- tests/e2e/integration/login.ts
+++ tests/e2e/integration/login.ts
@@ -3,7 +3,7 @@ describe("Login", () => {
     cy.visit("/");
     cy.findByText("Sign in").click();
     cy.findByRole("textbox", { name: /Email/ }).type("jonjongrim@gmail.com");
-    cy.findByRole("textbox", { name: /Password/ }).type(process.env.PLAYABL_PW);
+    cy.findByLabelText("Password").type(Cypress.env("PLAYABL_PW"));
     cy.url().should("eq", "http://localhost:3000/profile");
   });
 });
