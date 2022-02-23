e578559309318193896bfc340567ff3eac952983
Jon Grim <jonjongrim@gmail.com>
2022-02-23 14:59:18 -0500

    add icons and fetch handler (#13)
    * add icons and fetch handler
    
    * update badge

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ self.addEventListener("push", function (event) {
   const options = {
     body: event.data.text(),
     icon: "images/icon.png",
-    badge: "images/badge.png",
+    badge: "/playabl_192.png",
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
@@ -13,4 +13,27 @@ self.addEventListener("notificationclick", function (event) {
   event.notification.close();
 
   event.waitUntil(clients.openWindow("https://playabl.io"));
+});
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("PLAYABL-STATIC-CACHE").then(function (cache) {
+      return cache.addAll([
+        "/src/assets/Open_Sans/OpenSans-VariableFont.ttf",
+        "/src/assets/Paytone_One/PaytoneOne-Regular.ttf",
+        "/favicon.ico",
+      ]);
+    })
+  );
+});
+
+// The "network falling back to cache" strategy
+// https://web.dev/offline-cookbook/
+self.addEventListener("fetch", function (event) {
+  console.log(event);
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request);
+    })
+  );
 });
