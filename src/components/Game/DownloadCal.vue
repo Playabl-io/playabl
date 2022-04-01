bde84d81a89d0a064f6f53e25689e03b435e951e
Jonathan Grim <jonjongrim@gmail.com>
2022-03-31 20:43:02 -0400

    add to calendar

---
+++ src/components/Game/DownloadCal.vue
@@ -0,0 +1,42 @@
+<template>
+  <a
+    download
+    :href="`data:text/calendar;charset=utf8,${event}`"
+    class="flex items-center gap-2 p-2 bg-white rounded-md hover:shadow-md focus-styles text-sm font-medium"
+  >
+    <CalendarIcon class="w-6 h-6" />
+    Download iCalendar
+  </a>
+</template>
+<script setup lang="ts">
+import { computed } from "vue";
+import { format } from "date-fns";
+import { CalendarIcon } from "@heroicons/vue/outline";
+import { gameStore } from "@/pages/Game/gameStore";
+import * as ics from "ics";
+
+const props = defineProps({
+  startTime: {
+    type: Number,
+    required: true,
+  },
+  endTime: {
+    type: Number,
+    required: true,
+  },
+});
+
+const event = computed(() => {
+  const { value } = ics.createEvent({
+    start: format(props.startTime, "yyyy-M-d-H-m")
+      .split("-")
+      .map((val) => parseInt(val)),
+    end: format(props.endTime, "yyyy-M-d-H-m")
+      .split("-")
+      .map((val) => parseInt(val)),
+    title: gameStore.game.title,
+    description: `Game details: https://playabl.io/games/${gameStore.game.id}`,
+  });
+  return encodeURIComponent(value || "");
+});
+</script>
--- src/components/Game/SessionBlock.vue
+++ src/components/Game/SessionBlock.vue
@@ -1,64 +1,75 @@
 <template>
   <div class="flex flex-col">
-    <div class="p-4 rounded-md border border-solid border-gray-200 shadow-md">
-      <div class="grid grid-cols-2">
-        <div>
-          <p class="text-lg font-bold">
-            {{ format(new Date(session.start_time), "EEE, MMM do") }}
-          </p>
-          <p class="text-sm mt-2 text-slate-800">
-            {{ format(new Date(session.start_time), "h:mm a") }} -
-            {{ format(new Date(session.end_time), "h:mm a z") }}
-          </p>
-        </div>
-        <template v-if="!isOwner">
-          <GhostButton
-            v-if="userIsInTheGame"
-            :is-loading="isProcessing"
-            @click="handleLeave"
-          >
-            Leave session
-          </GhostButton>
-          <PrimaryButton
-            v-else-if="canRsvp"
-            class="self-center mx-4"
-            :is-loading="isProcessing"
-            @click="handleJoin"
-          >
-            Join
-          </PrimaryButton>
-          <div
-            v-else-if="soonestRsvp"
-            class="text-sm text-slate-700 text-center"
-          >
-            RSVP available {{ formatRelative(soonestRsvp, new Date()) }}
-          </div>
-          <p v-else class="text-center text-sm">
-            You cannot RSVP, likely because you need to be assigned an access
-            level.
-            <br />Please contact the community organizers.
-          </p>
-        </template>
+    <div class="p-4 rounded-md border border-solid border-gray-100 shadow-md">
+      <div>
+        <p class="text-lg font-bold">
+          {{ format(new Date(session.start_time), "EEE, MMM do") }}
+        </p>
+        <p class="text-sm text-slate-800">
+          {{ format(new Date(session.start_time), "h:mm a") }} -
+          {{ format(new Date(session.end_time), "h:mm a z") }}
+        </p>
       </div>
-    </div>
-    <div class="grid gap-4 mt-6">
-      <div class="col-span-2">
-        <h6 class="text-xs text-slate-600 mb-2">RSVP'd</h6>
-        <ul class="mb-4">
-          <SessionAttendee
-            v-for="rsvp in participants[0]"
-            :id="rsvp"
-            :key="rsvp"
-          />
-        </ul>
-        <h6 class="text-xs text-slate-600 mb-2">Waitlist</h6>
-        <ul>
-          <SessionAttendee
-            v-for="rsvp in participants[1]"
-            :id="rsvp"
-            :key="rsvp"
-          />
-        </ul>
+      <div v-if="!isOwner" class="mt-4 mb-8">
+        <SecondaryButton
+          v-if="userIsInTheGame"
+          :is-loading="isProcessing"
+          class="w-full"
+          @click="handleLeave"
+        >
+          Leave session
+        </SecondaryButton>
+        <PrimaryButton
+          v-else-if="canRsvp"
+          class="w-full"
+          :is-loading="isProcessing"
+          @click="handleJoin"
+        >
+          Join
+        </PrimaryButton>
+        <div v-else-if="soonestRsvp" class="text-sm text-slate-700 text-center">
+          RSVP available {{ formatRelative(soonestRsvp, new Date()) }}
+        </div>
+        <p v-else class="text-center text-sm">
+          You cannot RSVP, likely because you need to be assigned an access
+          level.
+          <br />Please contact the community organizers.
+        </p>
+      </div>
+      <div class="mt-8 grid grid-cols-2">
+        <div>
+          <h6 class="text-xs text-slate-600 mb-2">RSVP'd</h6>
+          <ul class="mb-4">
+            <SessionAttendee
+              v-for="rsvp in participants[0]"
+              :id="rsvp"
+              :key="rsvp"
+            />
+          </ul>
+        </div>
+        <div>
+          <h6 class="text-xs text-slate-600 mb-2">Waitlist</h6>
+          <ul>
+            <SessionAttendee
+              v-for="rsvp in participants[1]"
+              :id="rsvp"
+              :key="rsvp"
+            />
+          </ul>
+        </div>
+      </div>
+      <div
+        v-if="userIsInTheGame"
+        class="mt-4 grid content-center gap-2 p-4 rounded-md bg-gray-200"
+      >
+        <AddToGoogleCal
+          :start-time="session.start_time"
+          :end-time="session.end_time"
+        />
+        <DownloadCal
+          :start-time="session.start_time"
+          :end-time="session.end_time"
+        />
       </div>
     </div>
   </div>
@@ -74,9 +85,11 @@ import { CommunityAccess } from "@/typings/CommunityAccess";
 import { compareUserAccessToRsvpTimes, getSoonestRsvpTime } from "@/util/time";
 import { store } from "@/store";
 import useToast from "../Toast/useToast";
-import GhostButton from "../Buttons/GhostButton.vue";
 import { gameStore } from "@/pages/Game/gameStore";
 import SessionAttendee from "@/pages/Game/SessionAttendee.vue";
+import AddToGoogleCal from "./AddToGoogleCal.vue";
+import DownloadCal from "./DownloadCal.vue";
+import SecondaryButton from "../Buttons/SecondaryButton.vue";
 
 const { showSuccess, showError } = useToast();
 
