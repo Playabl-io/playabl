1536aad8a9a0af3688695bc63a937be331d7eb8e
Jon Grim <jonjongrim@gmail.com>
2022-10-23 13:14:29 -0400

    add Google to privacy (#59)

--- src/pages/PrivacyPolicy.vue
+++ src/pages/PrivacyPolicy.vue
@@ -1,13 +1,13 @@
 <template>
   <BaseTemplate>
     <Heading as="h1" level="h1">Privacy Policy</Heading>
-    <p class="text-xs text-slate-700 mt-2">Last updated: April 5th, 2022</p>
-    <p class="prose mt-24">
+    <p class="text-xs text-slate-700 mt-2">Last updated: October 23rd, 2022</p>
+    <p class="prose mt-12">
       The information we collect about you is not sold or transferred to any
       third party except to provide services to you as part of your use of the
       site. The following companies and services are used by Playabl:
     </p>
-    <ul class="mt-8 list-disc list-inside prose">
+    <ul class="mt-6 list-disc list-inside prose">
       <li>
         <a
           href="https://supabase.com/"
@@ -26,14 +26,28 @@
         >
         for transactional emails
       </li>
+      <li>
+        <a
+          href="https://aws.amazon.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+          >AWS</a
+        >
+        for application services and processing
+      </li>
     </ul>
-    <p class="prose mt-12">
+    <p class="prose mt-6">
+      If you choose to sign up or sign in with Google, your Google email is
+      shared with Playabl for purposes of authenticating you using Google's
+      service.
+    </p>
+    <p class="prose mt-6">
       The information you provide to Playabl — such as preferred name, pronouns,
       and email — should be considered public and shared with the communities
       you join to faciliate communications. Please only reveal information you
       are comfortable with having available to others.
     </p>
-    <p class="prose mt-12">
+    <p class="prose mt-6">
       For any questions or concerns, or to request account deletion, contact
       <a href="mailto:support@playabl.io">Playabl Support</a>.
     </p>
