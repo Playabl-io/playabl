ce80c9fdec2043f89ae842cf512a518eabf7db63
Jon Grim <jonjongrim@gmail.com>
2022-10-31 19:19:49 -0400

    add more clear messaging about RSVP status (#74)

--- src/pages/Game/GameHome.vue
+++ src/pages/Game/GameHome.vue
@@ -9,6 +9,17 @@
   <div v-if="gameStore.game?.description" class="p-8 bg-gray-100 rounded-lg">
     <TipTapDisplay :content="gameStore.game.description" />
   </div>
+  <div v-if="userIsNotMember" class="flex flex-col items-center my-12">
+    <p class="text-sm text-slate-700 font-semibold mb-3">
+      You must be a member of the community to RSVP
+    </p>
+    <PrimaryButton
+      :to="`/communities/${gameStore.game.community_id}`"
+      class="w-auto inline-block"
+    >
+      Learn more about {{ gameStore.community.name }}
+    </PrimaryButton>
+  </div>
 
   <section v-if="!gameStore.game.deleted_at" class="mt-12">
     <div class="grid md:grid-cols-2 gap-8">
@@ -19,13 +30,15 @@
         :participant-count="session.participant_count"
         :user-access="userAccess"
         :is-owner="isOwner"
+        :not-a-member="userIsNotMember"
       />
     </div>
   </section>
 </template>
 <script setup lang="ts">
-import { onMounted, ref, toRefs } from "vue";
+import { onMounted, ref, computed } from "vue";
 import TipTapDisplay from "@/components/TipTapDisplay.vue";
+import PrimaryButton from "@/components/Buttons/PrimaryButton.vue";
 import SessionBlock from "@/components/Game/SessionBlock.vue";
 import { loadUserCommunityAccess } from "@/api/communityAccess";
 import { store } from "@/store";
@@ -33,15 +46,14 @@ import { CommunityAccess } from "@/typings/CommunityAccess";
 import { getCoverImageUrl } from "@/api/storage";
 import { gameStore } from "./gameStore";
 
-const props = defineProps({
+defineProps({
   isOwner: {
     type: Boolean,
     required: true,
   },
 });
-toRefs(props);
 
-const userAccess = ref<CommunityAccess[]>();
+const userAccess = ref<CommunityAccess[]>([]);
 const gameCoverImage = ref("");
 
 onMounted(async () => {
@@ -50,6 +62,12 @@ onMounted(async () => {
     gameCoverImage.value = await getCoverImageUrl(gameStore.game.cover_image);
   }
 });
+
+const userIsNotMember = computed(() =>
+  userAccess.value.every(
+    (access) => access.community_id !== gameStore.game.community_id
+  )
+);
 
 async function getUserAccess() {
   if (!store.user) return;
