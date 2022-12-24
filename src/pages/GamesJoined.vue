42dae3f7148c240605777e8c041b204e2f728752
Jonathan Grim <jonjongrim@gmail.com>
2022-12-24 10:17:34 -0500

    update wording for clarity

--- src/pages/GamesJoined.vue
+++ src/pages/GamesJoined.vue
@@ -5,13 +5,22 @@
       <LoadingSpinner color="brand-500" />
     </div>
     <section v-else class="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-      <div v-if="games.length === 0" class="col-span-full">
-        <p class="prose">You do not have any upcoming games.</p>
+      <div
+        v-if="games.length === 0"
+        class="col-span-full flex flex-col items-start"
+      >
+        <p class="prose mb-3">You are not playing in any upcoming games.</p>
+        <router-link
+          to="/games/manage"
+          class="text-brand-500 font-semibold mb-1 border-b border-transparent hover:border-solid hover:border-brand-500"
+        >
+          Manage games you posted.
+        </router-link>
         <router-link
           to="/games/browse"
-          class="text-brand-500 font-semibold hover:border-b hover:border-solid hover:border-brand-500"
+          class="text-brand-500 font-semibold border-b border-transparent hover:border-solid hover:border-brand-500"
         >
-          You can browse available games here.
+          Browse upcoming games.
         </router-link>
       </div>
       <GameCard v-for="game in games" :key="game.id" :game="game" />
