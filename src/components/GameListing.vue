59bca92eb69c22b4f3a18f9a3ae71c93bc4c0447
Jonathan Grim <jonjongrim@gmail.com>
2023-01-13 22:10:38 -0500

    make the game image a link

--- src/components/GameListing.vue
+++ src/components/GameListing.vue
@@ -12,51 +12,53 @@
           {{ game.community_id.name }}
         </router-link>
       </div>
-      <div
-        class="w-full relative"
-        :class="{
-          'aspect-w-16 aspect-h-9': coverImageUrl,
-        }"
-      >
-        <img
-          v-if="coverImageUrl"
-          class="w-full h-full object-center object-cover shadow-md rounded-lg"
-          :src="coverImageUrl"
-          alt=""
-          loading="lazy"
-        />
+      <router-link :to="`/games/${game.id}`" :aria-label="`View ${game.title}`">
         <div
+          class="w-full relative"
           :class="{
-            'bg-gray-100': !coverImageUrl,
+            'aspect-w-16 aspect-h-9': coverImageUrl,
           }"
-          class="w-full h-full flex flex-col justify-end rounded-lg"
         >
-          <div class="flex flex-wrap gap-6 px-6 py-4">
-            <div
-              v-for="session in game.sessions"
-              :key="session.id"
-              class="p-2 rounded-md flex items-center space-x-1 shadow-sm"
-              :class="[session.has_openings ? 'bg-green-200' : 'bg-gray-200']"
-            >
-              <p class="text-sm font-semibold">
-                {{ format(new Date(session.start_time), "LLL do") }}
-              </p>
-              <Tooltip v-if="session.has_openings">
-                <template #tooltip>Seats available</template>
-                <template #trigger="{ toggleTooltip }">
-                  <StarIcon
-                    class="h-4 w-4 text-emerald-600"
-                    @mouseenter="toggleTooltip"
-                    @mouseleave="toggleTooltip"
-                    @focus="toggleTooltip"
-                    @blur="toggleTooltip"
-                  />
-                </template>
-              </Tooltip>
+          <img
+            v-if="coverImageUrl"
+            class="w-full h-full object-center object-cover shadow-md rounded-lg"
+            :src="coverImageUrl"
+            alt=""
+            loading="lazy"
+          />
+          <div
+            :class="{
+              'bg-gray-100': !coverImageUrl,
+            }"
+            class="w-full h-full flex flex-col justify-end rounded-lg"
+          >
+            <div class="flex flex-wrap gap-6 px-6 py-4">
+              <div
+                v-for="session in game.sessions"
+                :key="session.id"
+                class="p-2 rounded-md flex items-center space-x-1 shadow-sm"
+                :class="[session.has_openings ? 'bg-green-200' : 'bg-gray-200']"
+              >
+                <p class="text-sm font-semibold">
+                  {{ format(new Date(session.start_time), "LLL do") }}
+                </p>
+                <Tooltip v-if="session.has_openings">
+                  <template #tooltip>Seats available</template>
+                  <template #trigger="{ toggleTooltip }">
+                    <StarIcon
+                      class="h-4 w-4 text-emerald-600"
+                      @mouseenter="toggleTooltip"
+                      @mouseleave="toggleTooltip"
+                      @focus="toggleTooltip"
+                      @blur="toggleTooltip"
+                    />
+                  </template>
+                </Tooltip>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </router-link>
       <div class="flex flex-wrap gap-4">
         <GameBadge v-if="game.system" title="System" :value="game.system">
           <template #icon>
