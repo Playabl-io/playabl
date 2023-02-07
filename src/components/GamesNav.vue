ef9c7522e440d8578429047920c83b53fb6cd20a
Jon Grim <jonjongrim@gmail.com>
2023-02-07 14:58:37 -0500

    update wording (#107)

--- src/components/GamesNav.vue
+++ src/components/GamesNav.vue
@@ -20,7 +20,7 @@
     >
       Manage
     </router-link>
-    <router-link to="/games/new"> New </router-link>
+    <router-link to="/games/new"> Create New </router-link>
   </section>
 </template>
 <script setup lang="ts">
