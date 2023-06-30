d15893efc562dccd0b93ff15dc59a58f25fc559b
Jon Grim <jonjongrim@gmail.com>
2023-06-30 09:35:04 -0400

    update github files and index screen message (#129)

---
+++ .github/ISSUE_TEMPLATE/question.md
@@ -0,0 +1,9 @@
+---
+name: Question / Discussion
+about: Ask a question or start a discussion
+title: ""
+labels: "question"
+assignees: ""
+---
+
+To ask a question or discussion, please use [GitHub Discussions](https://github.com/Playabl-io/playabl/discussions) rather than posting an issue. Any questions or discussions posted to issues will be converted to a discussion topic. Thanks!
--- src/components/Navigation/AppFooter.vue
+++ src/components/Navigation/AppFooter.vue
@@ -2,20 +2,35 @@
   <footer class="grid grid-cols-2 md:grid-cols-4 gap-4 p-8 bg-gray-100 z-10">
     <div class="col-span-2 flex flex-col justify-between">
       <img src="/playabl_192.png" alt="Playabl logo" class="w-12 h-12 filter" />
-      <p class="text-xs text-slate-600 mt-2">© Playabl 2022</p>
+      <p class="text-xs text-slate-600 mt-2">© Playabl 2022 - 2023</p>
     </div>
-    <div class="flex flex-col">
-      <p class="font-semibold mb-4">Support</p>
-      <a href="https://docs.playabl.io" class="text-sm hover:underline mb-2">
+    <div class="flex flex-col gap-2">
+      <p class="font-semibold">Support</p>
+      <a
+        target="_blank"
+        href="https://docs.playabl.io"
+        class="text-sm hover:underline"
+      >
         Documentation
       </a>
-      <a class="text-sm hover:underline" href="mailto:support@playabl.io">
-        Contact
+      <a
+        target="_blank"
+        class="text-sm hover:underline"
+        href="https://github.com/Playabl-io/playabl"
+      >
+        View on GitHub
+      </a>
+      <a
+        target="_blank"
+        class="text-sm hover:underline"
+        href="https://github.com/Playabl-io/playabl/issues"
+      >
+        File an issue, bug, or feature request
       </a>
     </div>
-    <div class="flex flex-col">
-      <p class="font-semibold mb-4">Legal</p>
-      <RouterLink to="/privacy" class="text-sm hover:underline mb-2">
+    <div class="flex flex-col gap-2">
+      <p class="font-semibold">Legal</p>
+      <RouterLink to="/privacy" class="text-sm hover:underline">
         Privacy Policy
       </RouterLink>
       <RouterLink to="/tos" class="text-sm hover:underline">
