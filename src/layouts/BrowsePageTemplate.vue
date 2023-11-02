feb5ee0fb4ec1c31705ccd1404f8ee169a659a51
Jonathan Grim <jonjongrim@gmail.com>
2023-11-02 09:46:31 +0100

    add a btn to scroll to top

--- src/layouts/BrowsePageTemplate.vue
+++ src/layouts/BrowsePageTemplate.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid sm:grid-cols-3 gap-6 h-full items-start">
-    <div class="flex flex-col gap-6">
+    <div class="flex flex-col h-full gap-6">
       <PrimaryButton
         v-if="allowCreateNew"
         class="shadow-sm"
@@ -23,6 +23,25 @@
           <slot name="page-controls"></slot>
         </template>
       </div>
+      <transition
+        enter-active-class="transition-all duration-300 ease-out"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class="transition-all duration-200 ease-in"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <button
+          v-if="backToTopVisible"
+          class="rounded-full shadow-lg w-8 h-8 bg-blue-500 grid place-content-center"
+          :class="[
+            isSmAndLarger ? 'sticky top-1/2' : 'fixed bottom-2 inset-x-2',
+          ]"
+          @click="scrollToTop"
+        >
+          <ArrowUpCircleIcon class="text-white w-8 h-8" />
+        </button>
+      </transition>
     </div>
 
     <div class="sm:col-span-2 rounded-md w-full">
@@ -31,12 +50,24 @@
   </div>
 </template>
 <script setup lang="ts">
+import { computed } from "vue";
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
-import { useBreakpoints, breakpointsTailwind } from "@vueuse/core";
+import { useBreakpoints, breakpointsTailwind, useScroll } from "@vueuse/core";
 import PrimaryButton from "@/components/Buttons/PrimaryButton.vue";
+import { ArrowUpCircleIcon } from "@heroicons/vue/24/solid";
 
 const breakpoints = useBreakpoints(breakpointsTailwind);
 const isSmAndLarger = breakpoints.greater("sm");
+
+const { y } = useScroll(window);
+
+const backToTopVisible = computed(() => {
+  return y.value > 750;
+});
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
 
 defineProps({
   allowCreateNew: {
@@ -51,11 +82,3 @@ defineProps({
 
 const emit = defineEmits(["createNew", "clear"]);
 </script>
-<style scoped>
-.controls {
-  max-height: 80%;
-}
-.controls {
-  max-height: 80%;
-}
-</style>
